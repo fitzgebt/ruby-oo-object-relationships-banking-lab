4428859f78d45eacad6a0bc76a4328a342871275
fitzgebt <fitzgebt@gmail.com>
2021-01-26 11:25:42 -0800

    commit tree 8b5e330236c7b8064d22de4b6a4b7ec7da19a693

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,36 @@
-class BankAccount
+require 'pry'
 
+class BankAccount
+    attr_accessor :balance, :status
+    attr_writer
+    attr_reader :name
+
+    #@@all = []
+
+    def initialize(name)
+        @name=name
+        @balance=1000
+        @status = "open"
+        #@@all << self
+    end
+
+    def deposit(amount)
+        self.balance = self.balance + amount
+    end
+
+    def display_balance
+       "Your balance is $#{self.balance}."
+    end
+
+    def valid?
+        self.status == "open" && self.balance > 0
+    end
+
+    def close_account
+        self.status = "closed"
+    end
+
+    def self.all
+        @@all
+    end
 end
